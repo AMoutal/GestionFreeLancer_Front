d5c1d0ff0a019bec0dd8a5c5a2540728e0dcf8f0
tcs0686a <touch.sovannroth@gmail.com>
2022-03-03 18:37:20 +0100

    vue page test

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -31,17 +31,3 @@ export class LoginComponent implements OnInit, OnDestroy
     }
 
 }
-
-
-/**
- export class LoginComponent {
-credentials = {username:'',password: ''};
-constructor(private appService:AppService, private
-httpClient:HttpClient,private router:Router){}
-login(){
-this.appService.authenticate(this.credentials,()=>{this.router.navigateByU
-rl("/base/users")});
-return false;
-}
-}
- */
